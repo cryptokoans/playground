81ce0e1eb6edc40375fa828e267d6f1a1011843c
cryptokoans <cryptokoans@protonmail.com>
2019-03-07 00:13:48 -0800

    just playing around with some contexts

--- src/main/scala/com/cryptokoans/Main.scala
+++ src/main/scala/com/cryptokoans/Main.scala
@@ -14,19 +14,18 @@ import fluence.crypto.hash.JsCryptoHasher
 import scodec.bits.ByteVector
 
 object Main {
-  /*def main(args: scala.Array[String]): Unit = {
-    val secret = label(string("secret","secret"),"my secret")
-    val hashedSecret = secret.map(s => ByteVector(JsCryptoHasher.Sha256.unsafe(s.getBytes)).toHex)
-    renderOutput(hashedSecret,"#app").runSyncStep
-  }*/
 
   def main(args: scala.Array[String]): Unit = {
-    val hashedSecret = label(string("hashed secret", "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"),"Hashed Secret")
-    val secret = label(string("secret",""),"Secret")
+    val hashedSecret = label(string("hashed secret", "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"),"Hashed Secret (hex encoded)")
+    val secret = label(string("secret","abc"),"Secret (string)")
     //val secretText = hashedSecret flatMap(s => text(s))
     val candidate = secret map hashString
     val verified = (hashedSecret,secret) parMapN hashverify
-    render(hashedSecret &> secret &> output(hashedSecret) &> output(candidate) &> output(verified), "#app").runSyncStep
+    val another = JsCryptoHasher.Sha256[Owlet]("abc".getBytes).map(ByteVector(_).toHex).value.flatMap{
+      case Left(e) => text(e.message)
+      case Right(v) => text(v)
+    }
+    render(div(hashedSecret) &> div(secret) &> output(hashedSecret) &> output(candidate) &> output(verified) &> another, "#app").runSyncStep
   }
 
   def hashString(input: String): String = ByteVector(JsCryptoHasher.Sha256.unsafe(input.getBytes)).toHex
